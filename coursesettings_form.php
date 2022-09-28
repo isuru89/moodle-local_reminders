ee42eb256950dc731c21c8c3badf93be977bbe9c
Shubhendra Doiphode <doiphode.sunny@gmail.com>
2022-09-28 23:28:13 +0100

    Added system timezone option

--- coursesettings_form.php
+++ coursesettings_form.php
@@ -107,7 +107,7 @@ class local_reminders_coursesettings_edit_form extends moodleform {
                 get_string('activityconfupcomingactivitiesdesc', 'local_reminders'));
 
             $daytimeformat = get_string('strftimedaydate', 'langconfig');
-            $tzone = core_date::get_user_timezone($USER);
+            $tzone = reminders_get_timezone($USER);
             foreach ($upcomingactivities as $daytime => $dailyactivities) {
                 $mform->addElement('static', 'header'.$daytime, '<h4><b>'.userdate($daytime, $daytimeformat, $tzone).'</b></h4>');
                 $isfirstone = true;
