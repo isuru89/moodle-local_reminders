4bb8751b48caaffc090d171ee5da7850f8b743c5
isuru <isuruw89@gmail.com>
2024-04-02 08:23:58 +0530

    fix cron failure of unable to print  #179

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024011200;
+$plugin->version   = 2024040200;
 $plugin->requires  = 2018051700;        // Require moodle 3.5 or higher.
-$plugin->release   = '2.7.1';
+$plugin->release   = '2.7.2';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'local_reminders';
